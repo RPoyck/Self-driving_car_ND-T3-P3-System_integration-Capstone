00cf56f757dc29687ed5ad76171b8fa52d0a982e
Rob Poyck <rob.poyck@streetscooter.eu>
2018-10-06 22:23:39 +0200

    Working waypoint follower version

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,47 +26,41 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 
 
 class WaypointUpdater(object):
-
-
-    #-------------#
-	# Constructor #
-	#-------------#
-    def __init__(self):
-		
-        rospy.init_node('waypoint_updater')
+	def __init__(self):
+		rospy.init_node('waypoint_updater')
 		
 		# ROS subscribers #
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+		rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+		rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
-        # TODO: Add other member variables you need below
-        self.pose = None
-        self.base_waypoints = None
-        self.waypoint_2d = None
-        self.waypoint_tree = None
-
-        #rospy.spin()
-        self.loop()
+		# TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
 
+		self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-    #---------#
-    # Methods #
-    #---------#
+		# TODO: Add other member variables you need below
+		self.pose = None
+		self.base_waypoints = None
+		self.waypoints_2d = None
+		self.waypoint_tree = None
 
-    # When the complete list of waypoints have been received and stored, get and publish the closest waypoints with a frequency of 30 [hz] #
-    def loop(self):
+		#rospy.spin()
+		self.loop()
+
+
+
+	#---------#
+	# Methods #
+	#---------#
+
+	# When the complete list of waypoints have been received and stored, get and publish the closest waypoints with a frequency of 30 [hz] #
+	def loop(self):
 		rate = rospy.Rate(30)
 		while not rospy.is_shutdown():
 			if self.pose and self.base_waypoints:
-				# Get closest waypoint #
-				closest_waypoint_idx = self.get_closest_waypoint_idx()
+				closest_waypoint_idx = self.get_closest_waypoint_idx()	# Get closest waypoint #
 				self.publish_waypoints(closest_waypoint_idx)
+				
 			rate.sleep()
 
 
@@ -74,16 +68,16 @@ class WaypointUpdater(object):
 		x_veh = self.pose.pose.position.x
 		y_veh = self.pose.pose.position.y
 		# Get the ID of the one waypoint which is closest to the vehicle position #
-		closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+		closest_idx = self.waypoint_tree.query([x_veh, y_veh], 1)[1]
 		
 		# Check if this closest waypoint is ahead or behind the vehicle #
-		closest_cood = self.waypoints_2d[closest_idx]
+		closest_coord = self.waypoints_2d[closest_idx]
 		prev_coord = self.waypoints_2d[closest_idx-1]
 		
 		# Equation for hyperplane through closest_coords #
 		cl_vect = np.array(closest_coord)
 		prev_vect = np.array(prev_coord)
-		pos_vect = np.array([x, y])
+		pos_vect = np.array([x_veh, y_veh])
 		
 		# Dot product of the vectors of the two coords #
 		val = np.dot( cl_vect - prev_vect, pos_vect - cl_vect )
@@ -102,52 +96,52 @@ class WaypointUpdater(object):
 		self.final_waypoints_pub.publish(lane)
 
 
-    def get_waypoint_velocity(self, waypoint):
-        return waypoint.twist.twist.linear.x
+	def get_waypoint_velocity(self, waypoint):
+		return waypoint.twist.twist.linear.x
 
 
-    def set_waypoint_velocity(self, waypoints, waypoint, velocity):
-        waypoints[waypoint].twist.twist.linear.x = velocity
+	def set_waypoint_velocity(self, waypoints, waypoint, velocity):
+		waypoints[waypoint].twist.twist.linear.x = velocity
 
 
-    def distance(self, waypoints, wp1, wp2):
-        dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-            wp1 = i
-        return dist
+	def distance(self, waypoints, wp1, wp2):
+		dist = 0
+		dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+		for i in range(wp1, wp2+1):
+			dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
+			wp1 = i
+		return dist
 
 
 	#------------------#
-    # Callback methods #
-    #------------------#
+	# Callback methods #
+	#------------------#
 
 	# Receives and stores the current pose of the vehicle #
-    def pose_cb(self, msg):
-        self.pose = msg
+	def pose_cb(self, msg):
+		self.pose = msg
 
 
 	# Receives the one-time message with all waypoints and saves it within a KDTree member variable for later use #
-    def waypoints_cb(self, waypoints):
+	def waypoints_cb(self, waypoints):
 		self.base_waypoints = waypoints
 		if not self.waypoints_2d:
 			self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
 			self.waypoint_tree = KDTree(self.waypoints_2d)
 			
 			
-    def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+	def traffic_cb(self, msg):
+		# TODO: Callback for /traffic_waypoint message. Implement
+		pass
 	
 
-    def obstacle_cb(self, msg):
-        # TODO: Callback for /obstacle_waypoint message. We will implement it later
-        pass
+	def obstacle_cb(self, msg):
+		# TODO: Callback for /obstacle_waypoint message. We will implement it later
+		pass
 
 
 if __name__ == '__main__':
-    try:
-        WaypointUpdater()
-    except rospy.ROSInterruptException:
-        rospy.logerr('Could not start waypoint updater node.')
+	try:
+		WaypointUpdater()
+	except rospy.ROSInterruptException:
+		rospy.logerr('Could not start waypoint updater node.')
